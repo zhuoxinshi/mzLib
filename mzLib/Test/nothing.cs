2c7f79a9f71d670d380661db903f8e483b9ba033
Zhuoxin Shi <zshi94@wisc.edu>
2025-08-07 11:49:43 -0500

    .

--- mzLib/Test/nothing.cs
+++ mzLib/Test/nothing.cs
@@ -5,17 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 using Readers;
 using NUnit.Framework;
+using MassSpectrometry;
+using System.IO;
 
 namespace Test
 {
     public class nothing
     {
         [Test]
-        public static void TestReading()
+        public static void RNA()
         {
-
+            var path1 = @"E:\Temp\20250612_21mer#1-Am.raw";
+            var path2 = @"E:\Temp\20250612_22mer#1-m6Am.raw";
+            var path3 = @"E:\Temp\20250612_22mer#1-unmod.raw";
+            var path4 = @"E:\Temp\20250612_RNA-Mix_10V.raw";
+            var path5 = @"E:\Temp\20250612_RNA-Mix_30V.raw";
+            var path6 = @"E:\Temp\20250612-22mer#1-Am.raw";
+            var path7 = @"E:\Temp\20250612-22mer#1-m6A.raw";
+            var path8 = @"E:\Temp\20250616_21mer#1-Am.raw";
+            var paths = new List<string> { path1, path2, path3, path4, path5, path6, path7, path8 };
+            var dictionary = new Dictionary<string, MsDataScan[]>();
+            foreach (var path in paths)
+            {
+                var msData = MsDataFileReader.GetDataFile(path);
+                var scans = msData.GetAllScansList().Where(S => S.MsnOrder == 2).ToArray();
+                dictionary[Path.GetFileNameWithoutExtension(path)] = scans;
+            }
             var osmPath = @"E:\Temp\AllOSMs.osmtsv";
             var allOsms = SpectrumMatchTsvReader.ReadOsmTsv(osmPath, out List<string> warnings).ToList();
+
+            var cidEnergys = new List<string>();
+            foreach (var osm in allOsms)
+            {
+                var ms2Scan = dictionary[osm.FileName].First(s => s.OneBasedScanNumber == osm.Ms2ScanNumber);
+
+            }
         }
     }
 }
