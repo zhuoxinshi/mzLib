f889a9e43214a0289ef0e22e41f4255025ee4762
Zhuoxin Shi <zshi94@wisc.edu>
2025-08-07 11:47:44 -0500

    .

---
+++ mzLib/Test/nothing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Readers;
+using NUnit.Framework;
+
+namespace Test
+{
+    public class nothing
+    {
+        [Test]
+        public static void TestReading()
+        {
+
+            var osmPath = @"E:\Temp\AllOSMs.osmtsv";
+            var allOsms = SpectrumMatchTsvReader.ReadOsmTsv(osmPath, out List<string> warnings).ToList();
+        }
+    }
+}
