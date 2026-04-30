1a2f0e073450966de53673d50e93e52da0bb651b
Zhuoxin Shi <zshi94@wisc.edu>
2026-04-30 17:58:29 -0500

    .

--- mzLib/Test/Islet_PTM/Occupancy.cs
+++ mzLib/Test/Islet_PTM/Occupancy.cs
@@ -29,10 +29,10 @@ namespace Test.Islet_PTM
 
             string allPSMs_path = @"E:\Islets\Brian_data\PTM\MS3_all_LFgptmdFilterPrunedDb\Task1-SearchTask\AllPSMs.psmtsv";
             var allPSMsTMT_file = new PsmFromTsvFile(allPSMs_path);
-            var allPsms = allPSMsTMT_file.Results.Where(p => p.QValue <= 0.01 && p.DecoyContamTarget == "T");
+            var allPsms = allPSMsTMT_file.Results.Where(p => p.QValue <= 0.01 && p.DecoyContamTarget == "T" && !p.FullSequence.Contains("|"));
             var allPeptides_path = @"E:\Islets\Brian_data\PTM\MS3_all_LFgptmdFilterPrunedDb\Task1-SearchTask\AllPeptides.psmtsv";
             var allPeptidesTMT_file = new PsmFromTsvFile(allPeptides_path);
-            var allPeptides = allPeptidesTMT_file.Results.Where(p => p.QValue <= 0.01 && p.DecoyContamTarget == "T");
+            var allPeptides = allPeptidesTMT_file.Results.Where(p => p.QValue <= 0.01 && p.DecoyContamTarget == "T" && !p.FullSequence.Contains("|"));
             var allPeptidesWithMod = allPeptides.Where(p => SpectrumMatchFromTsv.ParseModifications(p.FullSequence).Values.Any(v => !notInteresting.Any(key => v.Contains(key))));
             
             var allPtms = new List<PtmRecord>();
@@ -47,25 +47,28 @@ namespace Test.Islet_PTM
                     allPtms.Add(new PtmRecord { Accession = peptide.ProteinAccession, ModSite = modSite, Mod = mod.Value });
                 }
             }
-
-            var allSitePsmGroups = new List<PtmSiteGroup>();
-            foreach (var ptm in allPtms.Distinct())
-            {
-                var sitePsms = new List<PsmFromTsv>();
-                var allPsmsFromTheProtein = allPsms.Where(p => p.ProteinAccession == ptm.Accession);
-                foreach(var psm in allPsmsFromTheProtein)
-                {
-                    int startAA = int.Parse(psm.StartAndEndResiduesInProtein.Split()[0].Split("[")[1]);
-                    int endAA = int.Parse(psm.StartAndEndResiduesInProtein.Split()[2].Split("]")[0]);
-                    if ((ptm.ModSite >= startAA && ptm.ModSite <= endAA) || (ptm.ModSite == 0 && startAA == 1))
-                    {
-                        var allModsFromThisPsm = SpectrumMatchFromTsv.ParseModifications(psm.FullSequence).Where(kvp => !notInteresting.Any(key => kvp.Value.Contains(key)));
-                        if (allModsFromThisPsm.Any(kvp => kvp.Key + startAA == ptm.ModSite && kvp.Value == ptm.Mod) || (ptm.ModSite == 0 && !allModsFromThisPsm.Any()))
-                        sitePsms.Add(psm);
-                    }
-                }
-                //allSitePsmGroups.Add(new PtmSiteGroup { PTM = ptm, AllPsms = sitePsms });
-            }
+            var allPtmGroups = allPtms.GroupBy(ptm => new {a = ptm.Accession, m = ptm.ModSite}).ToList();
         }
+
+
     }
+
+    //var allSitePsmGroups = new List<PtmSiteGroup>();
+    //        foreach (var ptm in allPtms.Distinct())
+    //        {
+    //            var sitePsms = new List<PsmFromTsv>();
+    //var allPsmsFromTheProtein = allPsms.Where(p => p.ProteinAccession == ptm.Accession);
+    //            foreach(var psm in allPsmsFromTheProtein)
+    //            {
+    //                int startAA = int.Parse(psm.StartAndEndResiduesInProtein.Split()[0].Split("[")[1]);
+    //int endAA = int.Parse(psm.StartAndEndResiduesInProtein.Split()[2].Split("]")[0]);
+    //                if ((ptm.ModSite >= startAA && ptm.ModSite <= endAA) || (ptm.ModSite == 0 && startAA == 1))
+    //                {
+    //                    var allModsFromThisPsm = SpectrumMatchFromTsv.ParseModifications(psm.FullSequence).Where(kvp => !notInteresting.Any(key => kvp.Value.Contains(key)));
+    //                    if (allModsFromThisPsm.Any(kvp => kvp.Key + startAA == ptm.ModSite && kvp.Value == ptm.Mod) || (ptm.ModSite == 0 && !allModsFromThisPsm.Any()))
+    //                    sitePsms.Add(psm);
+    //                }
+    //            }
+    //            //allSitePsmGroups.Add(new PtmSiteGroup { PTM = ptm, AllPsms = sitePsms });
+    //        }
 }
